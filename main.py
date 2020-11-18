c7e17cd13f5f58e4a0e5bc33e95a547d3c109999
rustymagnet3000 <rustymagnet3000@gmail.com>
2020-11-18 11:43:47 +0000

    Updated init method to deal with X509 or Byte buffer leaf cert

--- main.py
+++ main.py
@@ -14,20 +14,23 @@ from test_certs import root_ca_cert_pem
 
 
 class CertificateChecker:
-    def __init__(self, untrusted_cert_pem):
-        self.root_cert = load_certificate(FILETYPE_PEM, root_ca_cert_pem)
-        self.int_cert = load_certificate(FILETYPE_PEM, int_ca_cert_pem)
-        self.untrusted_cert = load_certificate(FILETYPE_PEM, untrusted_cert_pem)
+    def __init__(self, untrusted_leaf):
+        self.trusted_certs = X509Store()
         self.load_trust_store()
+        if isinstance(untrusted_leaf, OpenSSL.crypto.X509):
+            self.untrusted_leaf = untrusted_leaf
+        elif isinstance(untrusted_leaf, bytes):
+            self.untrusted_leaf = load_certificate(FILETYPE_PEM, untrusted_leaf)
 
     def load_trust_store(self):
-        self.trusted_certs = X509Store()
-        self.trusted_certs.add_cert(self.root_cert)
-        self.trusted_certs.add_cert(self.int_cert)
+        root_cert = load_certificate(FILETYPE_PEM, root_ca_cert_pem)
+        int_cert = load_certificate(FILETYPE_PEM, int_ca_cert_pem)
+        self.trusted_certs.add_cert(root_cert)
+        self.trusted_certs.add_cert(int_cert)
 
     def verify_cert(self):
         try:
-            store_ctx = X509StoreContext(self.trusted_certs, self.untrusted_cert)
+            store_ctx = X509StoreContext(self.trusted_certs, self.untrusted_leaf)
             store_ctx.verify_certificate()
             return True
         except OpenSSL.crypto.X509StoreContextError as e:
@@ -54,6 +57,33 @@ class CertificateChecker:
         print(s)
 
     @staticmethod
+    def get_leaf_cert_from_host(hostname: str):
+        '''
+            Create Stream socket and connects.  Blocking.  This is a connection oriented socket
+            The SSLContext.wrap_socket()method returns an SSLSocket.
+            upgrade the socket to TLS without any certificate verification, to obtain the certificate in bytes
+        '''
+        #
+        dest = (hostname, 443)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(2)
+        sock.connect(dest)
+
+        try:
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = True
+            ctx.verify_mode = ssl.CERT_REQUIRED
+            sock = ctx.wrap_socket(sock, server_hostname=dest[0])
+            der_cert_bytes = sock.getpeercert(True)
+            leaf_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, der_cert_bytes)
+            return leaf_cert
+        except:
+            print("exception throw")
+            return None
+        finally:
+            sock.close()
+
+    @staticmethod
     def openssl_version():
         return "OpenSSL: {openssl}\ncryptography: {cryptography}".format(
             openssl=OpenSSL.SSL.SSLeay_version(OpenSSL.SSL.SSLEAY_VERSION),
@@ -76,47 +106,23 @@ class TestCertificateChecker(unittest.TestCase):
 
     def test_no_int_ca_in_trust_store(self):
         check = CertificateChecker(good_leaf_cert_pem)
-        check.trusted_certs = X509Store()
-        check.trusted_certs.add_cert(check.root_cert)
+        check.trusted_certs = X509Store()       # None out the Trust Store
+        root_cert = load_certificate(FILETYPE_PEM, root_ca_cert_pem)
+        check.trusted_certs.add_cert(root_cert)
         self.assertFalse(check.verify_cert(), "Expected to fail verify, as Int CA was removed")
 
     def test_openssl_types(self):
         check = CertificateChecker(bad_leaf_cert_pem)
         self.assertTrue(isinstance(check.trusted_certs, OpenSSL.crypto.X509Store))
-        self.assertTrue(isinstance(check.untrusted_cert, OpenSSL.crypto.X509))
+        self.assertTrue(isinstance(check.untrusted_leaf, OpenSSL.crypto.X509))
 
 
 if __name__ == '__main__':
-    print(CertificateChecker.openssl_version())
-    # tests = TestCertificateChecker()
-    # unittest.main()
-    # unittest.main(tests.test_no_int_ca_in_trust_store())
-    # check = CertificateChecker(good_leaf_cert_pem)
-    # CertificateChecker.print_cert_info(check.root_cert)
+    # print(CertificateChecker.openssl_version())
+    # hostname = 'httpbin.org'
+    # untrusted_leaf = CertificateChecker.get_leaf_cert_from_host(hostname)
+    # checker = CertificateChecker(untrusted_leaf)
+    # checker.verify_cert()
 
-
-    # Create Stream socket and connect ( blocking )
-    hostname = 'httpbin.org'
-    dest = (hostname, 443)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(2)
-    sock.connect(dest)
-
-    # Given a connection oriented socket, the SSLContext.wrap_socket()method returns an SSLSocket.
-    # upgrade the socket to TLS without any certificate verification, obtain the certificate in bytes
-    try:
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = True
-        ctx.verify_mode = ssl.CERT_REQUIRED
-        sock = ctx.wrap_socket(sock, server_hostname=dest[0])
-        cert_bin = sock.getpeercert(True)
-        der_cert_bytes = sock.getpeercert(True)
-        leaf_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, der_cert_bytes)
-        assert(isinstance(leaf_cert, OpenSSL.crypto.X509))
-        checker = CertificateChecker(leaf_cert)
-
-    except:
-        print("exception throw")
-    finally:
-        sock.close()
-
+    tests = TestCertificateChecker()
+    unittest.main(tests.test_openssl_types())
