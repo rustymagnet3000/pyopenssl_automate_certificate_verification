396c5f3c5d37ec27851e7945a8daef681fd06622
rustymagnet3000 <rustymagnet3000@gmail.com>
2020-11-25 12:06:19 +0000

    tidying up init to make code more readable

--- main.py
+++ main.py
@@ -19,44 +19,61 @@ from support.CertCheck import CertificateChecker
 from support.PyOpenSSLUnitTests import TestCertificateChecker
 
 
-class VerifySetup:
+class Verifier:
     results = {}
 
-    def __init__(self, ca_dir=Path(getcwd() + '/support/ca_files'), c_rehash=environ['HOME'] + '/openssl/bin/c_rehash'):
-        self.path_to_ca_certs = ca_dir
-        self.path_to_c_rehash = c_rehash
-        self.verify_flags = 0x80000                     # partial Chain allowed
+    def __init__(self, ca_dir=Path(getcwd() + '/support/ca_files'),
+                 c_rehash_loc=environ['HOME'] + '/openssl/bin/c_rehash'):
+        self.path_to_ca_certs = Verifier.verify_ca_dir_and_files(ca_dir)
+        self.path_to_c_rehash = Verifier.check_c_rehash_exists(c_rehash_loc)
+        self.verify_flags = 0x80000  # partial Chain allowed
         self.cert_hash_count = 0
         self.context = self.set_context()
-        self.verify_ca_dir_and_files()
+        self.run_c_rehash()
 
-    def auto_run_c_rehash(self):
+    @staticmethod
+    def check_c_rehash_exists(c_rehash_location):
         """
-            OpenSSL ships with /bin/c_rehash
+            OpenSSL ships /bin/c_rehash     Function checks it exists
+        """
+        if not exists(c_rehash_location):
+            print('[!]Cannot find c_rehash at:\t{0}'.format(c_rehash_location))
+            return None
+        return c_rehash_location
+
+    @staticmethod
+    def verify_ca_dir_and_files(ca_dir):
+        """
+            Check CA directory exists
+        """
+        if not exists(ca_dir) and not isdir(ca_dir):
+            print('[!]CA Directory of certificates not found:\t{0}'.format(ca_dir))
+            return None
+        return ca_dir
+
+    def run_c_rehash(self):
+        """
             rehash scans directories and calculates a hash value of each ".pem", ".crt", ".cer", or ".crl" file
-            Using a new Python import to do the same
         """
-        if exists(self.path_to_c_rehash):
-            process = subprocess.Popen(['~/openssl/bin/c_rehash', self.path_to_ca_certs],
+
+        process = subprocess.Popen([self.path_to_c_rehash, self.path_to_ca_certs],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
-            stdout, stderr = process.communicate()
-            print(stdout, stderr)
-        else:
-            print('[!]Cannot find c_rehash at:{}'.format(self.path_to_c_rehash))
+        stdout, stderr = process.communicate()
+        if stderr.__len__() > 0 or stdout.__len__() == 0:
+            print('[*]Error during c_rehash step:\t{0}'.format(stderr))
             return None
+        print('[*]c_rehash:\t{0}'.format(stdout))
 
-    def verify_ca_dir_and_files(self):
-        """
-            Check directory exists and contain symbolic link files from c_rehash
-        """
-        if exists(self.path_to_ca_certs) and isdir(self.path_to_ca_certs):
-            self.auto_run_c_rehash()
-            for file in listdir(self.path_to_ca_certs):
-                if file.endswith('.0'):
-                    self.cert_hash_count += 1
-            if self.cert_hash_count > 0:
-                print('[*]Found {0} certificate hash values in path:{1}'.format(self.cert_hash_count, self.path_to_ca_certs))
+        for file in listdir(self.path_to_ca_certs):
+            if file.endswith('.0'):
+                self.cert_hash_count += 1
+        if self.cert_hash_count > 0:
+            print('[*]Found {0} certificate hash values in path:{1}'.format(self.cert_hash_count,
+                                                                            self.path_to_ca_certs))
+
+
+
 
     @staticmethod
     def verify_cb(conn, cert, err_num, depth, ok):
@@ -64,9 +81,9 @@ class VerifySetup:
             Callback that holds the Cert Chain verify result
         """
         if ok:
-            VerifySetup.results[cert.get_subject().CN] = 'VERIFIED'
+            Verifier.results[cert.get_subject().CN] = 'VERIFIED'
         if not ok:
-            VerifySetup.results[cert.get_subject().CN] = {'[!]verify failed:{1}'.format(depth, err_num)}
+            Verifier.results[cert.get_subject().CN] = {'[!]verify failed:{1}'.format(depth, err_num)}
         return ok
 
     def set_context(self):
@@ -77,7 +94,7 @@ class VerifySetup:
         con.set_options(OP_NO_SSLv2 | OP_NO_SSLv3 | OP_NO_TLSv1)
         con.get_cert_store().set_flags(self.verify_flags)
         con.load_verify_locations(cafile=None, capath=self.path_to_ca_certs.__bytes__())
-        con.set_verify(VERIFY_PEER, VerifySetup.verify_cb)
+        con.set_verify(VERIFY_PEER, Verifier.verify_cb)
         return con
 
 
@@ -85,28 +102,28 @@ if __name__ == '__main__':
     print(CertificateChecker.openssl_version())
     hosts = ['stackoverflow.com', 'httpbin.org']
     port = 443
-    ver_setup = VerifySetup()
-
-    # for host in hosts:
-    #     des = (host, port)
-    #     sock = socket()
-    #     sock.setblocking(True)
-    #     sock.connect_ex(sock.getsockname())
-    #     tls_client = Connection(ver_setup.context, sock)
-    #     tls_client.set_connect_state()  # set to work in client mode
-    #     print('\n[*]Setting up socket to:{}'.format(host))
-    #     sock.connect(des)
-    #     print('[*]connected: {0}\t{1}'.format(host, sock.getpeername()))
-    #     tls_client.do_handshake()
-    #     try:
-    #         print("")
-    #     except WantReadError:
-    #         print("[!]WantReadError")
-    #     except Error as e:
-    #         print("[!]OpenSSL.SSL.Error {0}", e)
-    #     except:
-    #         print("[!]general exception")
-    #     finally:
-    #         sock.close()
-    # print(VerifySetup.results)
-
+    verifier = Verifier()
+    if verifier.cert_hash_count > 0:
+        print("good shape")
+    for host in hosts:
+        des = (host, port)
+        sock = socket()
+        sock.setblocking(True)
+        sock.connect_ex(sock.getsockname())
+        tls_client = Connection(verifier.context, sock)
+        tls_client.set_connect_state()  # set to work in client mode
+        print('\n[*]Setting up socket to:{}'.format(host))
+        sock.connect(des)
+        print('[*]connected: {0}\t{1}'.format(host, sock.getpeername()))
+        tls_client.do_handshake()
+        try:
+            print("")
+        except WantReadError:
+            print("[!]WantReadError")
+        except Error as e:
+            print("[!]OpenSSL.SSL.Error {0}", e)
+        except:
+            print("[!]general exception")
+        finally:
+            sock.close()
+    print(Verifier.results)
